d694c5ae8455e368adb2c70a84bfb2c71974965a
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-04-15 22:24:18 +0900

    ユーザーデフォルトを保存/読み込み

--- UserDefaults2/UserDefaults2/ViewController.swift
+++ UserDefaults2/UserDefaults2/ViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // スライダとラベルをOutlet接続する
+    // スライダとボタンをOutlet接続する
     @IBOutlet weak var mySlider: UISlider!
     @IBOutlet weak var sliderLabel: UILabel!
     
-    // プログレスバーとラベルをOutlet接続する
+    // プログレスバーとボタンをOutlet接続する
     @IBOutlet weak var myProgress: UIProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     
+    // ユーザーデフォルトに保存する
+    @IBAction func saveValue(_ sender: Any) {
+        // ユーザーデフォルトを参照する
+        let defaults = UserDefaults.standard
+        let value = mySlider.value
+        // ラベルに値を表示する
+        sliderLabel.text = String(value)
+        // キー"sliderValue"で値を保存する
+        defaults.set(value, forKey: "sliderValue")
+    }
     
+    // ユーザーデフォルトから読み込む
+    @IBAction func readValue(_ sender: Any) {
+        // ユーザーデフォルトを参照する
+        let defaults = UserDefaults.standard
+        // キー"sliderValue"をFloatとして値を読み込む
+        let value = defaults.float(forKey: "sliderValue")
+        // ラベルに値を表示する
+        progressLabel.text = String(value)
+        // プログレスバーに値を設定する
+        myProgress.progress = value
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
